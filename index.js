752965e7bbfdd99206ba903aaf82b8cfd99569a1
Daniel Hron <daniel.hron@ales.aero>
2022-06-25 23:56:27 +0200

    zapis rozdilu do souboru

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const he = require('./helper')
 const bm = require('./burses/bitmart')
 const gi = require('./burses/gateio')
 const me = require('./burses/mexc')
+const fse = require('fs-extra');
 const BursesComparator = require('./BursesPriceComparator')
 const PairsDepthComparator = require('./PairsDepthComparator')
 
@@ -62,6 +63,7 @@ tickersComparefinishedCallback = async function(priceDifferences) {
 pairsComparefinishedCallback = function(compare) {
   console.log("\nGET DEPTH OF MARKETS FINISHED SUCCESSFUL")
 
+  var output = ""
   for(const pair of compare)
   {
     if(pair.buys.length > 0 && pair.sells.length > 0)
@@ -69,17 +71,28 @@ pairsComparefinishedCallback = function(compare) {
         var diff = 100-((pair.buys[0].price / pair.sells[0].price)*100)
         if(diff > 2 && diff < 100)
         {
-            console.log("*********************************************************************************************")
-            console.log("BUY AT: " + pair.buyBurse + " FOR: " + pair.buys[0].price + " AMOUNT: " + pair.buys[0].amount)
-            console.log("SELL AT: " + pair.sellBurse + " FOR: " + pair.sells[0].price + " AMOUNT: " + pair.sells[0].amount)
-            const sellFor = pair.sells[0].amount * pair.sells[0].price
-            const buyMax = Math.min(pair.sells[0].amount, pair.buys[0].amount)
-            console.log("YOU CAN SELL FOR: " + sellFor + "$ MAX BUY: " + buyMax)
-            console.log("DEPTH DIFFERENCE FOR: "  + pair.pair + " IS " + diff)
-            console.log("*********************************************************************************************")
+          
+          output += "*********************************************************************************************\n"
+          output += "BUY AT: " + pair.buyBurse + " FOR: " + pair.buys[0].price + " AMOUNT: " + pair.buys[0].amount + "\n"
+          output += "SELL AT: " + pair.sellBurse + " FOR: " + pair.sells[0].price + " AMOUNT: " + pair.sells[0].amount + "\n"
+          var sellFor = pair.sells[0].amount * pair.sells[0].price
+          var buyMax = Math.min(pair.sells[0].amount, pair.buys[0].amount)
+          output += "YOU CAN SELL FOR: " + sellFor + "$ MAX BUY: " + buyMax + "\n"
+          output += "DEPTH DIFFERENCE FOR: "  + pair.pair + " IS " + diff + "\n"
+          output += "*********************************************************************************************\n"
+
+          console.log("*********************************************************************************************")
+          console.log("BUY AT: " + pair.buyBurse + " FOR: " + pair.buys[0].price + " AMOUNT: " + pair.buys[0].amount)
+          console.log("SELL AT: " + pair.sellBurse + " FOR: " + pair.sells[0].price + " AMOUNT: " + pair.sells[0].amount)
+          sellFor = pair.sells[0].amount * pair.sells[0].price
+          buyMax = Math.min(pair.sells[0].amount, pair.buys[0].amount)
+          console.log("YOU CAN SELL FOR: " + sellFor + "$ MAX BUY: " + buyMax)
+          console.log("DEPTH DIFFERENCE FOR: "  + pair.pair + " IS " + diff)
+          console.log("*********************************************************************************************")
         }
     }
   }
+  fse.writeFileSync('./diffs.log', output);
 }
 
 function printProgress(progress) {
@@ -96,10 +109,11 @@ pairsCompareProgressCallback = function(progress) {
 bc = new BursesComparator(burses, tickersComparefinishedCallback);
 pc = new PairsDepthComparator(burses, pairsComparefinishedCallback, pairsCompareProgressCallback)
 
-timeout.timeoutAfter(10)
+timeout.timeoutAfter(60*5)
 console.log("GET TICKERS DIFFERENCES")
 bc.compare()
 
 function onTimeout() {
- // bc.compare(pairsToCompare)
+  console.log("GET TICKERS DIFFERENCES")
+  bc.compare()
 }
